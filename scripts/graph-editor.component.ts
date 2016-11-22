715d853f33b4ea5c7db226605aa43b2f2d0d1df9
CJ Dimaano <c.j.s.dimaano@gmail.com>
2016-11-21 19:38:32 -0700

    Editor component now draws circles and lines for nodes and edges.

--- scripts/graph-editor.component.ts
+++ scripts/graph-editor.component.ts
@@ -30,6 +30,10 @@ import {
   ViewChild
 } from "@angular/core";
 
+import { Graph } from "./graph";
+import { Node } from "./node";
+import { Edge } from "./edge";
+
 
 @Component({
   moduleId: module.id,
@@ -79,71 +83,43 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
    * dragObect
    *   The graph component being dragged by the cursor.
    */
-  private dragObect : any = null;
+  private dragObject : Node | Edge = null;
 
   /**
    * selectedItems
    *   The currently selected graph components.
    */
-  private selectedItems : Array<any> = [];
-
-  //
-  // TODO:
-  // Use actual types for nodes and edges.
-  //
+  private selectedItems : Array<Node | Edge> = [];
 
   /**
-   * _nodes
-   *    The list of nodes in the graph.
+   * _graph
+   *   The active graph being edited.
    */
-  private _nodes : Array<any> = [];
+  private _graph : Graph = null;
 
   /**
-   * _edges
-   *   The list of edges in the graph.
+   * graph
+   *   Sets the active graph.
    */
-  private _edges : Array<any> = [];
-
-  /**
-   * nodes
-   *   Sets the nodes of the graph and updates the canvas.
-   */
-  set nodes(value : Array<any>) {
-    this._nodes = value;
+  set graph(value : Graph) {
+    this._graph = value;
     this.redraw();
   }
 
   /**
-   * nodes
-   *   Gets the nodes of the graph.
+   * graph
+   *   Gets the active graph.
    */
-  get nodes() : Array<any> {
-    return this._nodes;
-  }
-
-  /**
-   * edges
-   *   Sets the edges of the graph and updates the canvas.
-   */
-  set edges(value : Array<any>) {
-    this._edges = value;
-    this.redraw();
-  }
-
-  /**
-   * edges
-   *   Gets the edges of the graph.
-   */
-  get edges() : Array<any> {
-    return this._edges;
+  get graph() : Graph {
+    return this._graph;
   }
 
   /**
    * dragNode
    *   Sets the node being dragged by the cursor.
    */
-  set dragNode(value : any) {
-    this.dragObect = value;
+  set dragNode(value : Node) {
+    this.dragObject = value;
   }
 
   /**
@@ -178,7 +154,7 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
     this.downEvent = e;
     this.isWaiting = true;
     //
-    // Set a timeout for 100ms.
+    // Set a timeout.
     //
     setTimeout(() => {
       //
@@ -186,16 +162,27 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
       // still set to true.
       //
       if(this.isWaiting) {
-        this.dragObect =
+        this.dragObject =
           this.hitTest(this.downEvent.clientX, this.downEvent.clientY);
-        if(this.dragObect === null) {
-          // TODO:
-          // Create node.
-          // Set drag object.
+        if(this.dragObject === null) {
+          //
+          // [Temporary] Create a new node and set it as the drag object.
+          //
+          let n = new Node(e.clientX, e.clientY);
+          this.dragObject = n;
+          this.drawNode(n);
+        }
+        else if(this.dragObject instanceof Node) {
+          this.dragObject.x = this.downEvent.clientX;
+          this.dragObject.y = this.downEvent.clientY;
+          this.redraw();
+        }
+        else if(this.dragObject instanceof Edge) {
+          this.drawEdge(this.dragObject, this.downEvent.x, this.downEvent.y);
         }
         this.isWaiting = false;
       }
-    }, 100);
+    }, 300);
   }
 
   /**
@@ -226,39 +213,46 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
         //
         // Update the drag object.
         //
-        this.dragObect = this.hitTest(e.clientX, e.clientY);
+        this.dragObject = this.hitTest(e.clientX, e.clientY);
         //
         // Activate the selection box if no drag object was set.
         //
-        if(this.dragObect === null)
+        if(this.dragObject === null)
           this.isSelecting = true;
         //
         // Create a new edge and update the drag object if the drag object was
         // set to a node.
         //
-        else if(this.isNode(this.dragObect)) {
-          // TODO:
-          // Create a new edge.
-          // Set drag object.
+        else if(this.dragObject instanceof Node) {
+          //
+          // [Temporary] Create a new edge and set it as the drag object.
+          //
+          var edge = new Edge();
+          edge.left = this.dragObject as Node;
+          this.dragObject = edge;
         }
       }
       //
       // Update the canvas if waiting is not set.
       //
       else if(!this.isWaiting) {
-        if(this.isSelecting) {
-          // TODO:
-          // Update selection box.
-        }
-        else if(this.isNode(this.dragObect)) {
-          // TODO:
-          // Update node position.
-        }
-        else if(this.isEdge(this.dragObect)) {
-          // TODO:
-          // Update edge endpoint.
-        }
         this.redraw();
+        if(this.isSelecting) {
+          this.drawSelectionBox(
+            this.downEvent.clientX, this.downEvent.clientY,
+            e.clientX, e.clientY
+          );
+        }
+        else if(this.dragObject instanceof Node) {
+          var n = this.dragObject as Node;
+          n.x = e.clientX;
+          n.y = e.clientY;
+          this.drawNode(n);
+        }
+        else if(this.dragObject instanceof Edge) {
+          var ed = this.dragObject as Edge;
+          this.drawEdge(ed, e.clientX, e.clientY);
+        }
       }
     }
   }
@@ -271,8 +265,8 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
     //
     // Set the selected graph component if waiting is set.
     //
+    this.selectedItems = [];
     if(this.isWaiting) {
-      this.selectedItems = [];
       var hitComponent = this.hitTest(e.clientX, e.clientY);
       if(hitComponent !== null)
         this.selectedItems.push(hitComponent);
@@ -281,7 +275,6 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
     // Set the selected components if selecting is set.
     //
     else if(this.isSelecting) {
-      this.selectedItems = [];
       //
       // TODO:
       // Iterate through all components.
@@ -291,29 +284,35 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
     //
     // Drop the node if one is being dragged.
     //
-    else if(this.isNode(this.dragObect)) {
+    else if(this.dragObject instanceof Node) {
       //
       // TODO:
       // If default creation node is not set, drop context menu for creating a node.
       // Drop node at location.
       // Set selected.
       //
+      this._graph.nodes.push(this.dragObject);
     }
     //
     // Update the edge if one is being dragged.
     //
-    else if(this.isEdge(this.dragObect)) {
+    else if(this.dragObject instanceof Edge) {
       //
       // Check that the mouse was released at a node.
       //
       var hit = this.hitTest(e.clientX, e.clientY);
-      if(this.isNode(hit)) {
+      if(hit instanceof Node) {
         //
         // TODO:
         // If default creation edge is not set, drop context menu for creating an edge.
         // Set endpoint of edge.
         // Set selected.
         //
+        if(this.dragObject.left)
+          this.dragObject.right = hit;
+        else
+          this.dragObject.left = hit;
+        this._graph.edges.push(this.dragObject);
       }
     }
     //
@@ -322,7 +321,7 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
     this.downEvent = null;
     this.isWaiting = false;
     this.isSelecting = false;
-    this.dragObect = null;
+    this.dragObject = null;
     //
     // Redraw the canvas.
     //
@@ -351,6 +350,70 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
   }
 
   /**
+   * drawSelectionBox
+   *   Draws the selection box.
+   */
+  drawSelectionBox(x1 : number, y1 : number, x2 : number, y2 : number) : void {
+    var w = x2 - x1;
+    var h = y2 - y1;
+    this.ctx.strokeStyle = "#00a2e8";
+    this.ctx.fillStyle = "#00a2e8";
+    this.ctx.globalAlpha = 0.1;
+    if(w < 0 && h < 0)
+      this.ctx.fillRect(x2, y2, w * -1, h * -1);
+    else if(w < 0 && h >= 0)
+      this.ctx.fillRect(x2, y1, w * -1, h);
+    else if(w >= 0 && h < 0)
+      this.ctx.fillRect(x1, y2, w, h * -1);
+    else
+      this.ctx.fillRect(x1, y1, w, h);
+    this.ctx.globalAlpha = 1.0;
+    if(w < 0 && h < 0)
+      this.ctx.strokeRect(x2, y2, w * -1, h * -1);
+    else if(w < 0 && h >= 0)
+      this.ctx.strokeRect(x2, y1, w * -1, h);
+    else if(w >= 0 && h < 0)
+      this.ctx.strokeRect(x1, y2, w, h * -1);
+    else
+      this.ctx.strokeRect(x1, y1, w, h);
+  }
+
+  /**
+   * drawNode
+   *   Draws a node on the canvas.
+   * 
+   * TODO:
+   *   This needs to be able to handle custom node shapes/images.
+   */
+  drawNode(n : Node) : void {
+    this.ctx.fillStyle = "#fff200";
+    this.ctx.strokeStyle = "black";
+    this.ctx.beginPath();
+    this.ctx.arc(n.x, n.y, 20, 0, 2 * Math.PI, false);
+    this.ctx.fill();
+    this.ctx.stroke();
+  }
+
+  /**
+   * drawEdge
+   *   Draws an edge on the canvas.
+   * 
+   * TODO:
+   *   This needs to be able to handle custom edge drawing.
+   */
+  drawEdge(e : Edge, x? : number, y? : number) : void {
+    this.ctx.strokeStyle = "black";
+    if(x && y) {
+      if(e.left === null)
+        this.drawLine(x, y, e.right.x, e.right.y);
+      else
+        this.drawLine(e.left.x, e.left.y, x, y);
+    }
+    else
+      this.drawLine(e.left.x, e.left.y, e.right.x, e.right.y);
+  }
+
+  /**
    * drawLine
    *   Draws a line.
    */
@@ -358,7 +421,6 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
     this.ctx.beginPath();
     this.ctx.moveTo(x1, y1);
     this.ctx.lineTo(x2, y2);
-    this.ctx.strokeStyle = "solid black 2px";
     this.ctx.stroke();
   }
 
@@ -382,6 +444,21 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
    */
   redraw() : void {
     this.clear();
+    //
+    // TODO:
+    // Don't actually do this.
+    //
+    if(this._graph === null) {
+      this._graph = new Graph();
+    }
+    //
+    // TODO:
+    // Draw selected components differently.
+    //
+    for(let e of this._graph.edges)
+      this.drawEdge(e);
+    for(let n of this._graph.nodes)
+      this.drawNode(n);
   }
 
   /**
@@ -392,37 +469,32 @@ export class GraphEditorComponent implements AfterViewInit, AfterViewChecked {
    *   Nodes take priority over edges.
    * </p>
    */
-  private hitTest(x : number, y : number) : any {
+  private hitTest(x : number, y : number) : Node | Edge {
     //
     // TODO:
-    // Iterate through all components.
-    // Return the first component that is hit by (x, y)
+    // Make sure to handle hit testing of custom shapes.
     //
+    for(let n of this._graph.nodes) {
+      var dx = n.x - x;
+      var dy = n.y - y;
+      if(dx * dx + dy * dy <= 20 * 20)
+        return n;
+    }
+    for(let e of this.graph.edges) {
+      var lx = e.left.x;
+      var ly = e.left.y;
+      var rx = e.right.x;
+      var ry = e.right.y;
+      var x1 = (lx < rx ? lx : rx);
+      var y1 = (ly < ry ? ly : ry);
+      var x2 = (lx < rx ? rx : lx);
+      var y2 = (ly < ry ? ry : ly);
+      var m = (y2 - y1) / (x2 - x1);
+      var b = y1 - m * x1;
+      if(Math.abs(y - (m * x + b)) < 5)
+        return e;
+    }
     return null;
-  }
-
-  /**
-   * isNode
-   *   Checks if a object is a node.
-   */
-  private isNode(val : any) : boolean {
-    //
-    // TODO:
-    // Check equivalency from node list.
-    //
-    return false;
-  }
-
-  /**
-   * isEdge
-   *   Checks if an object is an edge.
-   */
-  private isEdge(val : any) : boolean {
-    //
-    // TODO:
-    // Check equivalency from edge list.
-    //
-    return false;
   }
 
 }
