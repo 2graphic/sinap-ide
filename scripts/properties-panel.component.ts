e3c549b5c277425a2930469a3ab203b4891c68ae
Sheyne Anderson <sheyne@sheyne.com>
2016-11-30 14:28:10 -0700

    add an interface for the properties panel component to the graph

--- scripts/properties-panel.component.ts
+++ scripts/properties-panel.component.ts
@@ -12,5 +12,50 @@ import { Component } from "@angular/core";
   templateUrl: "../html/properties-panel.component.html",
   styleUrls: [ "../styles/side-panel.component.css" ]
 })
+
 export class PropertiesPanelComponent {
+	private _selectedEntity : PropertiedEntity;
+	get selectedEntity(){
+		return this._selectedEntity;
+	}
+	set selectedEntity(e){
+		// READTHIS: to Daniel
+		// this tells us when things change
+		// may want to change this to just be a field and 
+		// use Angular to detect changes to it. 
+		this._selectedEntity = e;
+	}
+}
+
+export enum EntityKind {
+	General,
+	PluginGenerated
+}
+
+export class SinapType {
+	type : string;
+}
+
+export class SinapTypedValue {
+	v : string;
+}
+
+export class Property{
+	readonly name : string;
+	readonly kind : EntityKind;
+	readonly type : SinapType;
+	value : SinapTypedValue;
+
+	constructor(name: string, kind: EntityKind, type: SinapType, value : SinapTypedValue){
+		this.name = name;
+		this.kind = kind;
+		this.type = type;
+		this.value = value;
+	}
+}
+
+export interface PropertiedEntity {
+	readonly names : Iterable<string>;
+	readonly properties : Iterable<Property>;
+	property(name : string) : SinapType;
 }
