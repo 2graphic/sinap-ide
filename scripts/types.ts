a77eb7980b6d97ac18d1e22d207672d4e0d779c4
Sheyne Anderson <sheyne@sheyne.com>
2016-12-02 16:17:19 -0700

    Add some types

--- scripts/types.ts
+++ scripts/types.ts
@@ -1,8 +1,11 @@
 export class SinapType {
-  constructor(public type : string){}
+  constructor(public type : string){
+  }
 
+  typeEqual(t : SinapType) : boolean{
+  	return this.type == t.type;
+  }
 }
-
 
 export const SinapString = new SinapType("string");
 export const SinapNumber = new SinapType("number");
@@ -10,14 +13,20 @@ export const SinapBoolean = new SinapType("boolean");
 export const SinapNode = new SinapType("node");
 export const SinapEdge = new SinapType("edge");
 
-class SinapOptionalType extends SinapType {
+export class SinapOptionalType extends SinapType {
 	constructor(public baseType : SinapType){
 		super("optional(" + baseType.type + ")");
 	}
 }
 
-class SinapListType extends SinapType {
+export class SinapListType extends SinapType {
 	constructor(public baseType : SinapType){
 		super("listof(" + baseType.type + ")");
+	}
+}
+
+export class SinapUnionType extends SinapType {
+	constructor(public baseTypes : Array<SinapType>){
+		super("unionof(" + baseTypes.map(x=>x.type).join(",") + ")");
 	}
 }
