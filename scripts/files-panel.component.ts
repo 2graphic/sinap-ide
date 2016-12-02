586a5d4c9bcfc65d3da6411e78bb743c73f8b8f7
Daniel James <daniel.s.james@icloud.com>
2016-12-01 21:41:27 -0700

    Stucture changes. Including an empty file panel and the blue tool bar.

---
+++ scripts/files-panel.component.ts
@@ -0,0 +1,16 @@
+// File: files-panel.component.ts
+// Created by: Daniel James
+// Date created: December 1, 2016
+
+
+import { Component } from "@angular/core";
+
+
+@Component({
+  moduleId: module.id,
+  selector: "sinap-files-panel",
+  templateUrl: "../html/files-panel.component.html",
+  styleUrls: [ "../styles/side-panel.component.css" ]
+})
+export class FilesPanelComponent {
+}
--- scripts/main.component.ts
+++ scripts/main.component.ts
@@ -14,6 +14,7 @@ import { PluginService, Interpreter } from "./plugin.service"
 import { REPLComponent, REPLDelegate } from "./repl.component"
 import { PropertiesPanelComponent, Property } from "./properties-panel.component"
 import { Element, Graph } from "./graph"
+import { SideBarComponent } from "./side-bar.component"
 
 @Component({
   moduleId: module.id,
@@ -43,28 +44,8 @@ export class MainComponent implements OnInit, MenuEventListener, REPLDelegate {
   @ViewChild(PropertiesPanelComponent)
   private propertiesPanel: PropertiesPanelComponent;
 
-
-  // Declare icons for the sidebar.
-  private icons = [
-    {
-      path: "properties.svg",
-      name: "Properties",
-      active: true
-    },
-    {
-      path: "tools.svg",
-      name: "Tools",
-      active: false
-    },
-    {
-      path: "files.svg",
-      name: "Files",
-      active: false
-    }
-  ];
-  private propertiesIcon = this.icons[0];
-  private toolsIcon = this.icons[1];
-  // ------------------------------
+  @ViewChild(SideBarComponent)
+  private sideBar: SideBarComponent;
 
 
   newFile() {
