b7089d896a73a97a2e9f10803f9e198658aad1a3
Daniel James <daniel.s.james@icloud.com>
2017-01-31 21:51:03 -0700

    WindowService implements ModalService, new file dialog (#78)
    * Proof of concept getting mutliple windows working.
    
    * Move stuff around for better organization and make a ModalService interface that the window service implements.
    
    * Creating a mock of the new file window and cleaning up/commenting.
    
    * Cleanup and formatting
    
    * More cleanup. Add a closeModal method to the ModalService interface and make createModal also return the ModalInfo
    
    * Remove log statments.
    
    * Display a "Create new file" page instead of empty space if no tabs are open.
    Cleaning up as mentioned in PR and created a modal type. (Slide down modal or child window.)
    
    * formatting

---
+++ app/modal-windows/services/window.service.ts
@@ -0,0 +1,67 @@
+// File: window.service.ts
+// Created by: Daniel James
+// Date created: January 30, 2017
+//
+
+import { Injectable, NgZone } from '@angular/core';
+
+import { remote, ipcRenderer } from 'electron';
+import { ModalInfo, ModalService, ModalType } from './../../models/modal-window'
+
+@Injectable()
+export class WindowService implements ModalService {
+    private callbacks = new Map<Number, (data: any) => void>();
+    public windowInfo: ModalInfo | null; // Main window will be null
+
+    constructor(private _ngZone: NgZone) {
+        ipcRenderer.on("windowResult", (event, arg) => this.callback(arg as ModalInfo));
+
+        // TODO: sending synchronous IPC here could be a bad idea, a better idea would be to send it async and store windowInfo as a Promise
+        this.windowInfo = ipcRenderer.sendSync("getWindowInfo", remote.getCurrentWindow().id);
+    }
+
+    public createModal(selector: string, type: ModalType): [ModalInfo, Promise<any>] {
+        var modal: ModalInfo = ipcRenderer.sendSync('createWindow', selector, type);
+
+        return [modal, new Promise((resolve, reject) => {
+            this.callbacks.set(modal.id, resolve);
+        })];
+    }
+
+    /**
+     * Asks the Electron process to close this window.
+     * data can be null, and if so the Promise isn't resolved.
+     */
+    public closeModal(modal: ModalInfo, data: any | null) {
+        if (modal) {
+            modal.data = data;
+            ipcRenderer.send('windowResult', modal);
+        }
+    }
+
+    /**
+     * Closes the currently open window (wrapper for closeModal)
+     */
+    public closeWindow(data: any | null) {
+        if (this.windowInfo) {
+            this.closeModal(this.windowInfo, data);
+        }
+    }
+
+    /**
+     * A helper function for resolving the promises.
+     */
+    private callback(arg: ModalInfo) {
+        var callback = this.callbacks.get(arg.id);
+        if (callback) {
+            this.callbacks.delete(arg.id);
+
+            var c = callback; // hack
+            this._ngZone.run(() => {
+                if (arg.data) {
+                    c(arg.data);
+                } // else cancel or error?   
+            });
+        }
+    }
+}
