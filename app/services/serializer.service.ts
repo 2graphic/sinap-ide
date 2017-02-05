b9160923b95ec672322962893e62aea1e0863a95
slaymaker1907 <dyllongagnier@gmail.com>
2017-02-05 00:44:59 -0700

    Removed unused store stuff.

--- app/services/serializer.service.ts
+++ app/services/serializer.service.ts
@@ -3,62 +3,6 @@ import { PropertiedEntity, PropertyList, PropertiedEntityLists } from "../compon
 import * as Core from '../models/core'
 import { PluginService } from '../services/plugin.service'
 
-function isPropertiedEntity(src: any): src is PropertiedEntity {
-    return !!(src && src.drawableProperties && src.pluginProperties && src.entityName);
-}
-
-class Store {
-    private entities: PropertiedEntity[] = [];
-    transformed: any[] = [];
-
-    add(pe: PropertiedEntity) {
-        this.entities.push(pe);
-        this.transformed.push(null);
-        return this.entities.length - 1;
-    }
-
-    get(idx: number) {
-        return this.transformed[idx];
-    }
-
-    set(idx: number, value: any) {
-        return this.transformed[idx] = value;
-    }
-
-    transformer(key: string, value: any): any {
-        const index = this.entities.indexOf(value);
-        if (index != -1) {
-            return { "pointerTo": index };
-        }
-        if (isPropertiedEntity(value)) {
-            return serialize(value, this);
-        }
-        return value;
-    }
-}
-
-function serialize(entity: PropertiedEntity, store: Store): any {
-    const index = store.add(entity);
-
-    const result: { [a: string]: any; } = { 'entityKind': entity.entityKind };
-
-    for (const key of ['drawableProperties', 'pluginProperties'] as (keyof PropertiedEntityLists)[]) {
-        const list: any = {};
-
-        for (const pair of entity[key].properties) {
-            const element = entity[key].get(pair[0]);
-            if (element) {
-                list[pair[0]] = (JSON.parse(JSON.stringify(element, (k, v) => store.transformer(k, v))))
-            }
-        }
-
-        result[key] = list;
-    }
-
-    store.set(index, result);
-    return { "pointerTo": index };
-}
-
 @Injectable()
 export class SerializerService {
     constructor( @Inject(PluginService) private pluginService: PluginService) { }
