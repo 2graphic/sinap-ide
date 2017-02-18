c7705cf65c1269a7c57e95c49db6f53bfa2da97c
Daniel James <daniel.s.james@icloud.com>
2017-02-17 17:52:25 -0700

    Make start check for a valid DFA graph.

--- plugins/dfa-interpreter.ts
+++ plugins/dfa-interpreter.ts
@@ -29,18 +29,63 @@ export class State {
     }
 }
 
-export function start(input: DFAGraph, data: string): State | boolean {
-    for (const node of input.nodes){
-        if (node.isStartState){
-            return new State(node, data, "starting");
+function isEmpty(label?: string) {
+    if (label != undefined) {
+        if (label != "") {
+            return false;
         }
     }
-    throw "no start state";
+
+    return true;
+}
+
+export function start(input: DFAGraph, data: string): State | boolean {
+    let start: DFANode | null = null;
+    const accepts = new Set<DFANode>();
+
+    for (const node of input.nodes){
+        if (node.isStartState) {
+            if (!start) {
+                start = node;
+            } else {
+                throw new Error("Only one start state allowed");
+            }
+        }
+
+        if (node.isAcceptState) {
+            accepts.add(node);
+        }
+
+        if (node.children) {
+            let transitions = new Set<string>();
+                for (const edge of node.children) {
+                    if (isEmpty(edge.label)) {
+                        throw new Error("Lambda transition from " + node.label + " to " + edge.destination.label + " is not allowed");
+                    }
+                    if (edge.label.length > 1) {
+                        throw new Error("Edge " + edge.label + " must be one symbol");
+                    }
+                    if (transitions.has(edge.label)) {
+                        throw new Error("Nondeterministic edge " + edge.label + (isEmpty(node.label)? "" : (" from node: " + node.label)));
+                    }
+                    transitions.add(edge.label);
+                }
+        }
+    }
+
+    if (!start) {
+        throw new Error("Must have one start state");
+    }
+    if (accepts.size == 0) {
+        throw new Error("Must have at least one accept state");
+    }
+
+    return new State(start, data, "starting");
 }
 
 export function step(current: State): State | boolean {
     if (current.inputLeft.length === 0) {
-        return current.active.isAcceptState;
+        return current.active.isAcceptState == true;
     }
     const destinations = current.active.children
         .filter(edge => edge.label === current.inputLeft[0])
