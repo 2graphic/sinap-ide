d6c7a79d9971eb11f19e7bb5f35cfcc5e415fb12
Daniel James <daniel.s.james@icloud.com>
2017-03-27 19:07:37 -0600

    Fix Unions

--- app/components/types/union-type/union-type.component.ts
+++ app/components/types/union-type/union-type.component.ts
@@ -4,7 +4,7 @@
 //
 
 import { Component, Input, ViewChild, ElementRef } from "@angular/core";
-import { CoreValue, PluginTypeEnvironment, CoreUnionValue, isUnionType, makeValue } from "sinap-core";
+import { CoreValue, PluginTypeEnvironment, CoreUnionValue, isUnionType, valueWrap, makeValue, CorePrimitiveValue } from "sinap-core";
 
 @Component({
     selector: "sinap-union-type",
@@ -15,17 +15,35 @@ export class UnionTypeComponent {
     private _value: CoreUnionValue<PluginTypeEnvironment>;
     @Input() readonly: boolean = true;
 
-    private options = new Array<CoreValue<PluginTypeEnvironment>>();
+    private options = new Array<CorePrimitiveValue<PluginTypeEnvironment>>();
 
     @Input() set value(v: CoreValue<PluginTypeEnvironment>) {
         if (isUnionType(v.type)) {
             this._value = v as CoreUnionValue<PluginTypeEnvironment>;
             v.type.types.forEach((t) => {
-                this.options.push(makeValue(t, undefined, false));
-                // TODO: Make this work with none primitive values
+                const option = makeValue(t, undefined, false);
+                if (option instanceof CorePrimitiveValue) {
+                    this.options.push(option);
+                    // TODO: Make this work with none primitive values                    
+                } else {
+                    console.log(v, "Currently only support union's of primitive values.");
+                }
             });
         } else {
             console.log(v, " is not a CoreUnionValue");
         }
+    }
+
+    getValue() {
+        if (this._value) {
+            return (this._value.value as CorePrimitiveValue<PluginTypeEnvironment>).data;
+        }
+
+        return "";
+    }
+
+    setValue(value: any) {
+        const newValue = valueWrap(this._value.type.env, value, this._value.mutable);
+        this._value.value = newValue;
     }
 }
