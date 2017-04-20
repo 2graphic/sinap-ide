17b1c3d15b312770185a0ce9950bf3340130380d
slaymaker1907 <dyllongagnier@gmail.com>
2017-04-20 11:05:19 -0600

    Plugin hotloading (#304)
    * Got reloading working for the manager.
    
    * Added detection for closing windows.
    
    * Formatting stuff
    
    * Log errors.
    
    * Check if result.
    
    * CMD+Shift+R to refresh plugins (no visual indicator).
    
    * Edit button with some CSS polish.
    
    * No path is specified when running it from .app so full path for code

--- app/modal-windows/services/window.service.ts
+++ app/modal-windows/services/window.service.ts
@@ -7,10 +7,11 @@ import { Injectable, NgZone } from '@angular/core';
 
 import { remote, ipcRenderer } from 'electron';
 import { ModalInfo, ModalService, ModalType } from './../../models/modal-window';
+import { NodePromise } from "sinap-core";
 
 @Injectable()
 export class WindowService implements ModalService {
-    private callbacks = new Map<Number, (data: any) => void>();
+    private callbacks = new Map<Number, NodePromise<any>>();
     private _windowDelegate?: WindowDelegate;
 
     public set windowDelegate(delegate: WindowDelegate | undefined) {
@@ -25,7 +26,7 @@ export class WindowService implements ModalService {
     private queuedWindow?: ModalInfo;
 
     constructor(private _ngZone: NgZone) {
-        ipcRenderer.on("windowResult", (event, arg) => this.callback(arg as ModalInfo));
+        ipcRenderer.on("windowResult", (event, arg) => this.callback(null, arg as ModalInfo));
         ipcRenderer.on("newWindow", (event, arg) => {
             if (this._windowDelegate) {
                 this._windowDelegate.newWindow(arg as ModalInfo);
@@ -33,14 +34,15 @@ export class WindowService implements ModalService {
                 this.queuedWindow = arg as ModalInfo;
             }
         });
+        ipcRenderer.on("windowClosed", (event, arg) => this.callback("Window closed early.", arg));
     }
 
     public createModal(selector: string, type: ModalType, data?: any): [ModalInfo, Promise<any>] {
-        let modal: ModalInfo = ipcRenderer.sendSync('createWindow', selector, type, data);
+        const modal: ModalInfo = ipcRenderer.sendSync('createWindow', selector, type, data);
+        const result = new NodePromise<any>();
+        this.callbacks.set(modal.id, result);
 
-        return [modal, new Promise((resolve, reject) => {
-            this.callbacks.set(modal.id, resolve);
-        })];
+        return [modal, result.promise];
     }
 
     /**
@@ -62,16 +64,14 @@ export class WindowService implements ModalService {
     /**
      * A helper function for resolving the promises.
      */
-    private callback(arg: ModalInfo) {
+    private callback(err: any, arg: ModalInfo) {
         let callback = this.callbacks.get(arg.id);
         if (callback) {
             this.callbacks.delete(arg.id);
 
             let c = callback; // hack
             this._ngZone.run(() => {
-                if (arg.data) {
-                    c(arg.data);
-                } // else cancel or error?
+                c.cb(err, arg.data);
             });
         }
     }
