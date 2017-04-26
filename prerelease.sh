ea17a90832c828360aa9eb29a90fd29bf8ea3925
Daniel James <daniel.s.james@icloud.com>
2017-04-26 10:02:39 -0600

    Comment out singing for Mac apps so we can manually do it.

--- prerelease.sh
+++ prerelease.sh
@@ -1,10 +1,10 @@
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-	echo $CERTIFICATE_OSX_P12 | base64 --decode > certificate.p12
-	security create-keychain -p $KEYCHAIN_PASSWORD build.keychain
-	security default-keychain -s build.keychain
-	security unlock-keychain -p $KEYCHAIN_PASSWORD build.keychain
-	security import certificate.p12 -k build.keychain -P $CERT_PASSWORD -T /usr/bin/codesign
-	node package.js darwin
+# 	echo $CERTIFICATE_OSX_P12 | base64 --decode > certificate.p12
+# 	security create-keychain -p $KEYCHAIN_PASSWORD build.keychain
+# 	security default-keychain -s build.keychain
+# 	security unlock-keychain -p $KEYCHAIN_PASSWORD build.keychain
+# 	security import certificate.p12 -k build.keychain -P $CERT_PASSWORD -T /usr/bin/codesign
+# 	node package.js darwin
 else
 	curl https://raw.githubusercontent.com/electron-userland/electron-packager/master/test/ci/before_install.sh | bash 
 	node package.js win32,linux
